6c094b2cc9279501539f0ab71b59553299074594
moran levi <moranlevi@h-MacBook-Pro-sl-moran.local>
2020-03-03 11:21:41 +0200

    initial commit

---
+++ FitnessTranningApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessTranningApp
+//
+//  Created by moran levi on 03/03/2020.
+//  Copyright © 2020 M.Levi's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
